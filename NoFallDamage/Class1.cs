9a86bbf177825e59f7dc1351b1a0cd7b96dea63d
Jakob Lamb <jakoblamb@yahoo.com>
2023-12-02 00:34:21 -0600

    Add project files.

---
+++ NoFallDamage/Class1.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using NoFallDamage.Patches;
+
+namespace NoFallDamage
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class NoFallDamageClass : BaseUnityPlugin
+    {
+        private const string modGUID = "jaceob.NoFallDamage";
+        private const string modName = "No Fall Damage";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static NoFallDamageClass instance;
+
+        internal ManualLogSource logSource;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            logSource.LogInfo("No Fall Damage mod started!");
+
+            harmony.PatchAll(typeof(NoFallDamageClass));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
---
+++ NoFallDamage/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace NoFallDamage.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void noFallDamagePatch(ref float ___timeSinceTakingGravityDamage)
+        {
+            ___timeSinceTakingGravityDamage = 0f;
+        }
+    }
+}
